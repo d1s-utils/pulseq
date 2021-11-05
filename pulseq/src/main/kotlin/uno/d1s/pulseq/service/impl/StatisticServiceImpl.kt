ccc1e80ae0d1272e3b864c76bbd0d49492292773
Mikhail koshkin <~@d1s.uno>
2021-11-06 01:01:30 +0300

    allow users to disable/enable statistics

--- pulseq/src/main/kotlin/uno/d1s/pulseq/service/impl/StatisticServiceImpl.kt
+++ pulseq/src/main/kotlin/uno/d1s/pulseq/service/impl/StatisticServiceImpl.kt
@@ -2,6 +2,7 @@ package uno.d1s.pulseq.service.impl
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import uno.d1s.pulseq.configuration.property.StatisticsConfigurationProperties
 import uno.d1s.pulseq.exception.StatisticNotFoundException
 import uno.d1s.pulseq.service.StatisticService
 import uno.d1s.pulseq.statistic.Statistic
@@ -12,10 +13,21 @@ class StatisticServiceImpl : StatisticService {
     @Autowired
     private lateinit var statistics: List<Statistic>
 
+    @Autowired
+    private lateinit var statisticsConfigurationProperties: StatisticsConfigurationProperties
+
     override fun getAllStatistics(): List<Statistic> =
-        statistics
+        statistics.filter { stat ->
+            notExcluded(stat.identify)
+        }
 
     override fun getStatisticByIdentify(identify: String): Statistic =
-        statistics.firstOrNull { it.identify == identify }
-            ?: throw StatisticNotFoundException("Provided identify is not valid.")
+        statistics.firstOrNull {
+            it.identify == identify && notExcluded(identify)
+        } ?: throw StatisticNotFoundException("Provided identify is not valid or the statistic is disabled.")
+
+    private fun notExcluded(identify: String) =
+        statisticsConfigurationProperties.parsedExcludes().none {
+            it == identify
+        }
 }
