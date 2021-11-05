7d2c5874c1e7bdca81150f7cc7a72c3a0a791811
Mikhail koshkin <~@d1s.uno>
2021-11-05 04:57:40 +0300

    moved caching annotations to the service tier

--- pulseq/src/main/kotlin/uno/d1s/pulseq/repository/BeatRepository.kt
+++ pulseq/src/main/kotlin/uno/d1s/pulseq/repository/BeatRepository.kt
@@ -1,25 +1,13 @@
 package uno.d1s.pulseq.repository
 
-import org.springframework.cache.annotation.CacheConfig
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import uno.d1s.pulseq.constant.cache.CacheNameConstants
 import uno.d1s.pulseq.domain.Beat
-import java.util.*
 
 @Repository
-@CacheConfig(cacheNames = [CacheNameConstants.BEAT])
 interface BeatRepository : MongoRepository<Beat, String> {
 
-    @Cacheable
     fun findAllByDeviceIdEquals(id: String): List<Beat>
 
-    @Cacheable
     fun findAllByDeviceNameEqualsIgnoreCase(deviceName: String): List<Beat>
-
-    @Cacheable
-    override fun findById(id: String): Optional<Beat>
-
-    override fun findAll(): MutableList<Beat>
 }
--- pulseq/src/main/kotlin/uno/d1s/pulseq/service/impl/BeatServiceImpl.kt
+++ pulseq/src/main/kotlin/uno/d1s/pulseq/service/impl/BeatServiceImpl.kt
@@ -1,6 +1,8 @@
 package uno.d1s.pulseq.service.impl
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CacheConfig
+import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
@@ -19,6 +21,7 @@ import uno.d1s.pulseq.util.findClosestInstantToCurrent
 import kotlin.properties.Delegates
 
 @Service("beatService")
+@CacheConfig(cacheNames = [CacheNameConstants.BEAT])
 class BeatServiceImpl : BeatService {
 
     @Autowired
@@ -33,12 +36,14 @@ class BeatServiceImpl : BeatService {
     @Autowired
     private lateinit var eventPublisher: ApplicationEventPublisher
 
+    @Cacheable
     @Transactional(readOnly = true)
     override fun findBeatById(id: String): Beat =
         beatRepository.findById(id).orElseThrow {
             BeatNotFoundException("Could not find any beats with provided id.")
         }
 
+    @CachePut
     @Transactional
     override fun registerNewBeatWithDeviceIdentify(identify: String): Beat {
         var device: Device by Delegates.notNull()
@@ -76,6 +81,7 @@ class BeatServiceImpl : BeatService {
     override fun findAllBeatsByDeviceId(deviceId: String): List<Beat> =
         beatRepository.findAllByDeviceIdEquals(deviceId)
 
+    @Cacheable
     @Transactional(readOnly = true)
     override fun findAllBeatsByDeviceName(deviceName: String): List<Beat> =
         beatRepository.findAllByDeviceNameEqualsIgnoreCase(deviceName)
@@ -84,8 +90,8 @@ class BeatServiceImpl : BeatService {
     override fun findAllBeatsByDeviceIdentify(deviceIdentify: String): List<Beat> =
         deviceService.findDeviceByIdentify(deviceIdentify).beats ?: listOf()
 
+    @Cacheable
     @Transactional(readOnly = true)
-    @Cacheable(cacheNames = [CacheNameConstants.BEAT])
     override fun findAllBeats(): List<Beat> =
         beatRepository.findAll()
 
