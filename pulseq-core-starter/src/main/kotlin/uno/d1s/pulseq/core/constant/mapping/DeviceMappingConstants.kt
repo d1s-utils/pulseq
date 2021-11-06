fa9fc219f451921b302ded99c6f7ec983c548b48
Mikhail koshkin <~@d1s.uno>
2021-11-07 00:46:34 +0300

    updated `GET_DEVICE_BY_IDENTIFY` mapping to make others working

--- pulseq-core-starter/src/main/kotlin/uno/d1s/pulseq/core/constant/mapping/DeviceMappingConstants.kt
+++ pulseq-core-starter/src/main/kotlin/uno/d1s/pulseq/core/constant/mapping/DeviceMappingConstants.kt
@@ -3,7 +3,7 @@ package uno.d1s.pulseq.core.constant.mapping
 object DeviceMappingConstants {
     @Suppress("MemberVisibilityCanBePrivate")
     const val BASE = "${GlobalMappingConstants.API}/device"
-    const val GET_DEVICE_BY_IDENTIFY = "$BASE/{identify}"
+    const val GET_DEVICE_BY_IDENTIFY = "$BASE/identify/{identify}"
     const val GET_ALL_DEVICES = "$BASE/list"
     const val REGISTER_DEVICE = "$BASE/new"
 }
