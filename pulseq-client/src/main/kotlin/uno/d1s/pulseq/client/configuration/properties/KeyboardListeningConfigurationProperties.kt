21950b369d6e6b408eb238648cfc5bff95ff6e13
Mikhail koshkin <~@d1s.uno>
2021-11-08 00:40:56 +0300

    added timer keyboard listening mode

--- pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/configuration/properties/KeyboardListeningConfigurationProperties.kt
+++ pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/configuration/properties/KeyboardListeningConfigurationProperties.kt
@@ -2,9 +2,12 @@ package uno.d1s.pulseq.client.configuration.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import uno.d1s.pulseq.client.configuration.properties.model.CountDownLatchConfigurationModel
+import uno.d1s.pulseq.client.configuration.properties.model.TimerConfigurationModel
 
 @ConfigurationProperties("pulseq.client.keyboard-listening-mode")
-class KeyboardListeningModeConfigurationProperties(
+class KeyboardListeningConfigurationProperties(
     val countDownLatch: CountDownLatchConfigurationModel
-    = CountDownLatchConfigurationModel()
+    = CountDownLatchConfigurationModel(),
+    val timer: TimerConfigurationModel
+    = TimerConfigurationModel()
 )
---
+++ pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/listener/TimerKeyboardEventListener.kt
@@ -0,0 +1,59 @@
+package uno.d1s.pulseq.client.listener
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.ApplicationListener
+import org.springframework.stereotype.Component
+import uno.d1s.pulseq.client.configuration.properties.KeyboardListeningConfigurationProperties
+import uno.d1s.pulseq.client.event.KeyboardActivityDetectedEvent
+import uno.d1s.pulseq.client.service.BeatSenderService
+
+@Component
+@ConditionalOnProperty(
+    prefix = "pulseq.client.keyboard-listening-mode.timer",
+    name = ["enabled"]
+)
+class TimerKeyboardEventListener : ApplicationListener<KeyboardActivityDetectedEvent> {
+
+    @Autowired
+    private lateinit var beatSenderService: BeatSenderService
+
+    @Autowired
+    private lateinit var keyboardListeningConfigurationProperties: KeyboardListeningConfigurationProperties
+
+    private var keyTyped = false
+
+    private var jobStarted = false
+
+    private val delay
+        get() = keyboardListeningConfigurationProperties.timer.delay.toMillis()
+
+    override fun onApplicationEvent(event: KeyboardActivityDetectedEvent) {
+        println("got event")
+        if (!jobStarted) {
+            startJob()
+            println("job started")
+            jobStarted = true
+        }
+
+        keyTyped = true
+    }
+
+    private fun startJob() {
+        CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                if (keyTyped) {
+                    beatSenderService.sendBeat()
+                    keyTyped = false
+                }
+
+                println("delay")
+                delay(delay)
+            }
+        }
+    }
+}
