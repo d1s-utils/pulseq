08b9fed45acf3e9e5bb03447710fffdfd7aa9682
d1snin <~@d1s.uno>
2021-11-09 16:16:13 +0300

    using `WebApplicationType.NONE` for `pulseq-client`

--- pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/PulseqClientApplication.kt
+++ pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/PulseqClientApplication.kt
@@ -1,5 +1,6 @@
 package uno.d1s.pulseq.client
 
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class PulseqClientApplication
 
 fun main(args: Array<String>) {
-    runApplication<PulseqClientApplication>(*args)
+    runApplication<PulseqClientApplication>(*args) {
+        webApplicationType = WebApplicationType.NONE
+    }
 }
