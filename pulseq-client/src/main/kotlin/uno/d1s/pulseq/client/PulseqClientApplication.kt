392d463afe4443b456f017161158faf83500b3e2
d1snin <~@d1s.uno>
2021-11-10 12:55:25 +0300

    added `@ConditionalOnNotWebApplication` for `pulseq-client`

--- pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/PulseqClientApplication.kt
+++ pulseq-client/src/main/kotlin/uno/d1s/pulseq/client/PulseqClientApplication.kt
@@ -2,9 +2,11 @@ package uno.d1s.pulseq.client
 
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConditionalOnNotWebApplication
 class PulseqClientApplication
 
 fun main(args: Array<String>) {
