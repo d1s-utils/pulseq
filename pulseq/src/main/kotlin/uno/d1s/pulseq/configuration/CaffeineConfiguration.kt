5a4368c1c3b7b1596b04a362fdba01ec099b106d
d1snin <~@d1s.uno>
2021-12-11 16:52:53 +0300

    removed cache size limits for caffeine

--- pulseq/src/main/kotlin/uno/d1s/pulseq/configuration/CaffeineConfiguration.kt
+++ pulseq/src/main/kotlin/uno/d1s/pulseq/configuration/CaffeineConfiguration.kt
@@ -5,7 +5,6 @@ import org.springframework.cache.CacheManager
 import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.Duration
 
 @Configuration
 class CaffeineConfiguration {
@@ -13,8 +12,6 @@ class CaffeineConfiguration {
     @Bean
     fun caffeine(): Caffeine<Any, Any> = Caffeine.newBuilder()
         .weakKeys()
-        .expireAfterWrite(Duration.ofHours(2))
-        .maximumSize(250_000)
 
     @Bean
     fun cacheManager(): CacheManager = CaffeineCacheManager().apply {
